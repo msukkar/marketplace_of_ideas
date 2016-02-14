3dbf2b7c9671a7fb44c3d44052693232e9f26f65
Kienan Adams <kra8ff@virginia.edu>
2016-02-14 15:49:55 -0500

    Update models

--- marketplace_of_ideas/models.py
+++ marketplace_of_ideas/models.py
@@ -5,12 +5,20 @@ class User(models.Model):
 	first_name = models.CharField(max_length=50)
 	last_name = models.CharField(max_length=50)
 	password = models.CharField(max_length=50)
-	# password
 
 class Transaction(models.Model):
 	time = models.DateTimeField()
-	user = models.ForeignKey(User)
-	post = models.ForeignKey(Post)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	post = models.ForeignKey(Post, on_delete=models.CASCADE)
 
-class Post(models.Model):
+class BlogPost(models.Model):
 	title = models.TextField()
+	body = models.TextField()
+	authors = models.ManyToManyField(User)
+
+class Comment(models.Model):
+	post = models.ForeignKey(Post, on_delete=models.CASCADE)
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
+	text = models.TextField()
+
+
