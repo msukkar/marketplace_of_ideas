80dcc515ab75c0f6b93cc1cb968d1b02b4ab31e3
Kienan Adams <kra8ff@virginia.edu>
2016-02-14 16:14:10 -0500

    Update models and database diagram

--- marketplace_of_ideas/models.py
+++ marketplace_of_ideas/models.py
@@ -6,10 +6,16 @@ class User(models.Model):
 	last_name = models.CharField(max_length=50)
 	password = models.CharField(max_length=50)
 
+class Relationship(models.Model):
+	follower = models.OneToOneField(User)
+	following = models.OneToOneField(User)
+
 class Transaction(models.Model):
 	time = models.DateTimeField()
-	user = models.ForeignKey(User, on_delete=models.CASCADE)
-	post = models.ForeignKey(Post, on_delete=models.CASCADE)
+	payer = models.ForeignKey(User)
+	receiver = models.ForeignKey(User)
+	blog_post = models.ForeignKey(BlogPost)
+	amount = models.DecimalField(max_digits=10, decimal_places=2)
 
 class BlogPost(models.Model):
 	title = models.TextField()
@@ -17,7 +23,7 @@ class BlogPost(models.Model):
 	authors = models.ManyToManyField(User)
 
 class Comment(models.Model):
-	post = models.ForeignKey(Post, on_delete=models.CASCADE)
+	blog_post = models.ForeignKey(BlogPost, on_delete=models.CASCADE)
 	author = models.ForeignKey(User, on_delete=models.CASCADE)
 	text = models.TextField()
 
