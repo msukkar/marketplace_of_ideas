989bc3833e795befb81b0c25dda43f37675c9c66
Mario Sukkar <mario@vividcortex.com>
2016-02-15 15:59:08 -0500

    Capture id parameters

--- marketplace_of_ideas/urls.py
+++ marketplace_of_ideas/urls.py
@@ -18,19 +18,19 @@ from django.conf.urls import include, url
 urlpatterns = [
     url(r'^api/v1/users$', views.users),
     url(r'^api/v1/users/new$', views.users),
-    url(r'^api/v1/users/([0-9]+)$', views.users),
+    url(r'^api/v1/users/(?P<user_id>[0-9]+)$', views.users),
 
     url(r'^api/v1/transactions$', views.transactions),
     url(r'^api/v1/transactions/new$', views.transactions),
-    url(r'^api/v1/transactions/([0-9]+)$', views.transactions),
+    url(r'^api/v1/transactions/(?P<transaction_id>[0-9]+)$', views.transactions),
 
     url(r'^api/v1/posts$', views.posts),
     url(r'^api/v1/posts/new$', views.posts),
-    url(r'^api/v1/posts/([0-9]+)$', views.posts),
+    url(r'^api/v1/posts/(?P<post_id>[0-9]+)$', views.posts),
 
     url(r'^api/v1/comments$', views.comments),
     url(r'^api/v1/comments/new$', views.comments),
-    url(r'^api/v1/comments/([0-9]+)$', views.comments),
+    url(r'^api/v1/comments/(?P<comment_id>[0-9]+)$', views.comments),
 
     url(r'^$', views.test),
 ]
