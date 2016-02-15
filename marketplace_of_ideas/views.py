c8b90237347ec2394bff9d77dbbb3ddc38970f9a
Kienan Adams <kra8ff@virginia.edu>
2016-02-15 17:05:29 -0500

    Update get requests to handle id input

--- marketplace_of_ideas/views.py
+++ marketplace_of_ideas/views.py
@@ -4,9 +4,16 @@ from marketplace_of_ideas.models import *
 from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
-def users(request, user_id):
+def users(request, user_id=-1):
     if request.method == 'GET':
-        return JsonResponse(serializers.serialize('json', User.objects.all()), safe=False)
+        if user_id == -1:
+            return JsonResponse(serializers.serialize('json', User.objects.all()), safe=False)
+        else:
+            try:
+                user = User.objects.get(pk=user_id)
+                return JsonResponse(serializers.serialize('json', user), safe=False)
+            except User.DoesNotExist:
+                return JsonResponse({ 'success': False})
     elif request.method == 'POST':
         user = User(username=request.POST["username"], first_name=request.POST["first_name"], last_name=request.POST["last_name"], password=request.POST["password"])
         user.save()
@@ -17,9 +24,16 @@ def users(request, user_id):
     #elif request.method == 'PUT':
 
 @csrf_exempt
-def transactions(request, transaction_id):
+def transactions(request, transaction_id=-1):
     if request.method == 'GET':
-        return JsonResponse(serializers.serialize('json', Transaction.objects.all()), safe=False)
+        if transaction_id == -1:
+            return JsonResponse(serializers.serialize('json', Transaction.objects.all()), safe=False)
+        else:
+            try:
+                transaction = Transaction.objects.get(pk=transaction_id)
+                return JsonResponse(serializers.serialize('json', transaction), safe=False)
+            except Transaction.DoesNotExist:
+                return JsonResponse({ 'success': False})
     elif request.method == 'POST':
         transaction = Transaction(payer_id=request.POST["payer_id"], receiver_id=request.POST["receiver_id"], blog_post_id=request.POST["blog_post_id"], amount=request.POST["amount"])
         transaction.save()
@@ -30,9 +44,16 @@ def transactions(request, transaction_id):
     #elif request.method == 'PUT':
 
 @csrf_exempt
-def posts(request, post_id):
+def posts(request, post_id=-1):
     if request.method == 'GET':
-        return JsonResponse(serializers.serialize('json', BlogPost.objects.all()), safe=False)
+        if post_id == -1:
+            return JsonResponse(serializers.serialize('json', BlogPost.objects.all()), safe=False)
+        else:
+            try:
+                post_post = BlogPost.objects.get(pk=post_id)
+                return JsonResponse(serializers.serialize('json', blog_post), safe=False)
+            except BlogPost.DoesNotExist:
+                return JsonResponse({ 'success': False})
     elif request.method == 'POST':
         post = BlogPost(title=request.POST["title"], body=request.POST["body"], author_id=request.POST["author_id"])
         post.save()
@@ -43,9 +64,16 @@ def posts(request, post_id):
     #elif request.method == 'PUT':
 
 @csrf_exempt
-def comments(request, comment_id):
+def comments(request, comment_id=-1):
     if request.method == 'GET':
-        return JsonResponse(serializers.serialize('json', Comment.objects.all()), safe=False)
+        if comment_id == -1:
+            return JsonResponse(serializers.serialize('json', Comment.objects.all()), safe=False)
+        else:
+            try:
+                comment = Comment.objects.get(pk=comment_id)
+                return JsonResponse(serializers.serialize('json', comment), safe=False)
+            except Comment.DoesNotExist:
+                return JsonResponse({ 'success': False})
     elif request.method == 'POST':
         comment = Comment(blog_post_id=request.POST["blog_post_id"], author_id=request.POST["author_id"], text=request.POST["text"])
         comment.save()
