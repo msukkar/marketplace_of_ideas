72532610c0acbb93b7c0e279895703a7db7c39f3
Kienan <kradams912@gmail.com>
2016-02-28 15:47:18 -0500

    Finish setting up docker containers

--- experience_api/experience_api/views.py
+++ experience_api/experience_api/views.py
@@ -5,14 +5,14 @@ import requests
 @csrf_exempt
 def home(request):
 	if request.method == 'GET':
-		response = requests.get('http://localhost:8001/api/v1/posts')
+		response = requests.get('http://models-api:8000/api/v1/posts')
 		# Probably rearrange based on popularity, time, etc
-		return JsonResponse(response.json())
+		return JsonResponse(response.json(), safe=False)
 	return JsonResponse({ 'success': False })
 
 def post(request, post_id):
 	if request.method == 'GET':
-		url = 'http://localhost:8001/api/v1/posts/' + str(post_id)
+		url = 'http://models-api:8000/api/v1/posts/' + str(post_id)
 		response = request.get(url)
-		return JsonResponse(response.json())
+		return JsonResponse(response.json(), safe=False)
 	return JsonResponse({ 'success': False })
