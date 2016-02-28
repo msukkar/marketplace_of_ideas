c2e49092c04dacfc3e2fccaad9c25777c755b472
Kienan Adams <kra8ff@virginia.edu>
2016-02-28 16:03:10 -0500

    Render an actual template in frontend

--- frontend/frontend/views.py
+++ frontend/frontend/views.py
@@ -1,5 +1,8 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.template import loader
 import requests
 
 @csrf_exempt
@@ -7,7 +10,13 @@ def home(request):
 	if request.method == 'GET':
 		response = requests.get('http://exp-api:8000/experience/v1/home')
 		# Probably rearrange based on popularity, time, etc
-		return JsonResponse(response.json(), safe=False)
+		#return JsonResponse(response.json(), safe=False)
+		context = {
+			'response': response.json()
+		}
+		template = loader.get_template('start.html')
+		return HttpResponse(template.render(context, request))
+
 	return JsonResponse({ 'success': False })
 
 def post(request, post_id):
