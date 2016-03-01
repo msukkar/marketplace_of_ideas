9e3d8b78ce39933adcbf3135e06ec6f04e5433b3
Mario Sukkar <mariosukkar@gmail.com>
2016-02-29 23:18:48 -0500

    Add fixtures file

--- experience_api/experience_api/views.py
+++ experience_api/experience_api/views.py
@@ -30,11 +30,11 @@ def post(request, post_id):
 			post_id = str(post['pk'])
 			comments = requests.get('http://models-api:8000/api/v1/comments?post_id=' + post_id)
 			comments_object = json.loads(comments.json())
-			# for comment in comments_object:
-			# 	author_id = str(comment[0][0])
-			# 	author_response = requests.get('http://models-api:8000/api/v1/users/' + author_id)
-			# 	author_response_json = json.loads(author_response.json())[0]
-			# 	comment['fields']['author'] = author_response_json['fields']['first_name'] + ' ' + author_response_json['fields']['last_name']
+			for comment in comments_object:
+				author_id = str(comment[0][0])
+				author_response = requests.get('http://models-api:8000/api/v1/users/' + author_id)
+				author_response_json = json.loads(author_response.json())[0]
+				comment['fields']['author'] = author_response_json['fields']['first_name'] + ' ' + author_response_json['fields']['last_name']
 			post['fields']['comments'] = comments_object
 		response_json = json.dumps(response_object)
 		return JsonResponse(response_json, safe=False)
