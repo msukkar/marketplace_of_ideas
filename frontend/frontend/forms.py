a88b9af6cfe7388a432aa68034e07579fc14e5fc
Kienan <kradams912@gmail.com>
2016-04-01 16:32:26 -0400

    Add some addition classes and styles

--- frontend/frontend/forms.py
+++ frontend/frontend/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 
 class LoginForm(forms.Form):
-	username = forms.CharField(label="Username", max_length=50)
-	password = forms.CharField(label="Password", max_length=50, widget = forms.PasswordInput())
+	username = forms.CharField(label="Username", max_length=50, widget = forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
+	password = forms.CharField(label="Password", max_length=50, widget = forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
 
 class BlogPostForm(forms.Form):
 	title = forms.CharField(label="Title", max_length=200)
